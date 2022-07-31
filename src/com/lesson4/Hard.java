1254f90847fbfdf46f51c88f0fa95f3abd4277ca
Belovphotos <ivanincredible@gmail.com>
2022-08-01 02:12:53 +0300

    дз выполнено

--- src/com/lesson4/Hard.java
+++ src/com/lesson4/Hard.java
@@ -1,4 +1,31 @@
 package com.lesson4;
 
+import java.util.Arrays;
+import java.util.Random;
+
 public class Hard {
+    public static void main(String[] args) {
+        Random random = new Random();
+
+        int[] array = new int[10];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = random.nextInt(100);
+        }
+        System.out.println("New array without sort: " + Arrays.toString(array));
+
+//        Arrays.sort(array);
+//
+//        System.out.println("Array sorted by method \"sort\": " + Arrays.toString(array));
+
+        for (int i = array.length - 1; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+                if (array[j] > array[j + 1]) {
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+        System.out.println("Array sorted by loop: " + Arrays.toString(array));
+    }
 }
